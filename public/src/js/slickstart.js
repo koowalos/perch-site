9cc1550be82cc25f8d125768f415207849d91db1
Paweł Kownacki <koowal@gmail.com>
2020-05-14 17:45:54 +0100

    1st-stage

---
+++ public/src/js/slickstart.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".top-main-carousel").slick({
+    autoplay: true,
+    autoplaySpeed: 4500,
+    arrows: false,
+    infinite: true,
+    speed: 3500,
+    fade: true,
+    cssEase: "linear",
+  });
+});

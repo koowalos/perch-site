96f8d214d48ff8230dde486d6e2ecef6d4236628
Paweł Kownacki <koowal@gmail.com>
2020-05-17 18:25:07 +0100

    4th

--- public/src/js/hamburger.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("myNav");
-  if (x.className === "header-content-navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "header-content-navbar";
-  }
-}
